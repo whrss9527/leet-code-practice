3aa7a0805206d29cacacfe3ac47fbb2270374365
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2021-09-02 11:54:24 +0800

    剑指 Offer 22. 链表中倒数第k个节点 双指针

---
+++ src/sword_to_offer/_22_链表中倒数第k个节点.java
@@ -0,0 +1,43 @@
+package sword_to_offer;
+
+/***
+ * 剑指 Offer 22. 链表中倒数第k个节点
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+ *
+ * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+ *
+ *
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 k = 2.
+ *
+ * 返回链表 4->5.
+ */
+public class _22_链表中倒数第k个节点 {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode backNode = head;
+        ListNode firstNode = head;
+        for (int i = 0; i < k ; i++){
+            if(firstNode==null){
+                return null;
+            }
+            firstNode = firstNode.next;
+        }
+        while (firstNode != null) {
+            firstNode = firstNode.next;
+            backNode = backNode.next;
+        }
+        return backNode;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
