cab21557137fe256e33429ec0577279344413c95
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2021-09-03 19:12:19 +0800

    面试题_17_14_最小K个数 排序｜堆 双解

---
+++ src/_面试题/_17_14_最小K个数.java
@@ -0,0 +1,57 @@
+package _面试题;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/***
+ * 设计一个算法，找出数组中最小的k个数。以任意顺序返回这k个数均可。
+ *
+ * 示例：
+ *
+ * 输入： arr = [1,3,5,7,2,4,6,8], k = 4
+ * 输出： [1,2,3,4]
+ * 提示：
+ *
+ * 0 <= len(arr) <= 100000
+ * 0 <= k <= min(100000, len(arr))
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/smallest-k-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class _17_14_最小K个数 {
+    // 库函数，排序 copy
+    public int[] smallestK(int[] arr, int k) {
+        Arrays.sort(arr);
+        return Arrays.copyOfRange(arr,0,k);
+    }
+    // 堆
+    public int[] smallestK2(int[] arr, int k) {
+        int[] vec = new int[k];
+        if (k == 0) { // 排除 0 的情况
+            return vec;
+        }
+        // 维护一个容量为k的堆 由大到小排列
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            public int compare(Integer num1, Integer num2) {
+                return num2 - num1;
+            }
+        });
+        for (int i = 0; i < k; ++i) {
+            queue.offer(arr[i]);
+        }
+        // 每次遇到有比堆头小的元素，替换进去（不该在其中的元素被挤出来）
+        for (int i = k; i < arr.length; ++i) {
+            if (queue.peek() > arr[i]) {
+                queue.poll();
+                queue.offer(arr[i]);
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            vec[i] = queue.poll();
+        }
+        return vec;
+    }
+
+}
