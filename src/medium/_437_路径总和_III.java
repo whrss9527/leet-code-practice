922e64786268d546534dc1fa561f178779adcc1b
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2021-09-30 14:30:50 +0800

    437 路径总和 动态规划java

---
+++ src/medium/_437_路径总和_III.java
@@ -0,0 +1,52 @@
+package medium;
+
+public class _437_路径总和_III {
+    // 动态规划
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        int ret = rootSum(root, targetSum);
+        ret += pathSum(root.left, targetSum);
+        ret += pathSum(root.right, targetSum);
+        return ret;
+    }
+
+    public int rootSum(TreeNode root, int targetSum) {
+        int ret = 0;
+
+        // 不解
+        if (root == null) {
+            return 0;
+        }
+        int val = root.val;
+        if (val == targetSum) {
+            ret++;
+        }
+
+        ret += rootSum(root.left, targetSum - val);
+        ret += rootSum(root.right, targetSum - val);
+        return ret;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
