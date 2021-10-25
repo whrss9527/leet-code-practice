4aebe3e366353b0c68bd64bc977bc57da829f4c2
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2021-10-25 19:24:45 +0800

    240 搜索二维矩阵II_二分查找

---
+++ src/medium/_240_搜索二维矩阵II.java
@@ -0,0 +1,32 @@
+package medium;
+
+public class _240_搜索二维矩阵II {
+    // 二分查找
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // 按行遍历
+        for (int[] row : matrix) {
+            int index = search(row, target);
+            if (index >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 对每行进行二分
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
