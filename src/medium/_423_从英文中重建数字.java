86a4599669145b8a817c502db69abe354fb935de
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2021-11-24 19:19:04 +0800

    423_从英文中重建数字 java脑筋急转弯

---
+++ src/medium/_423_从英文中重建数字.java
@@ -0,0 +1,40 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _423_从英文中重建数字 {
+    public String originalDigits(String s) {
+        // 统计每一个数字的英文单词中单独出现的字母，再将有若干并集出现的字母去除(减掉)有单独出现字母的数字，剩下每个数字出现的次数
+        // 累加
+        Map<Character, Integer> c = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            c.put(ch, c.getOrDefault(ch, 0) + 1);
+        }
+
+        int[] cnt = new int[10];
+        cnt[0] = c.getOrDefault('z', 0);
+        cnt[2] = c.getOrDefault('w', 0);
+        cnt[4] = c.getOrDefault('u', 0);
+        cnt[6] = c.getOrDefault('x', 0);
+        cnt[8] = c.getOrDefault('g', 0);
+
+        cnt[3] = c.getOrDefault('h', 0) - cnt[8];
+        cnt[5] = c.getOrDefault('f', 0) - cnt[4];
+        cnt[7] = c.getOrDefault('s', 0) - cnt[6];
+
+        cnt[1] = c.getOrDefault('o', 0) - cnt[0] - cnt[2] - cnt[4];
+
+        cnt[9] = c.getOrDefault('i', 0) - cnt[5] - cnt[6] - cnt[8];
+
+        StringBuffer ans = new StringBuffer();
+        for (int i = 0; i < 10; ++i) {
+            for (int j = 0; j < cnt[i]; ++j) {
+                ans.append((char) (i + '0'));
+            }
+        }
+        return ans.toString();
+    }
+
+}
