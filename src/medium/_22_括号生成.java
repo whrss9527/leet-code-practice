4f117cf76eaea77576083e4f142cc6f84d73b9d4
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2022-03-03 19:28:03 +0800

    22.括号生成

--- src/medium/_22_括号生成.java
+++ src/medium/_22_括号生成.java
@@ -1,4 +1,36 @@
 package medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _22_括号生成 {
+
+    ArrayList[] cache = new ArrayList[100];
+
+    public List<String> generate(int n) {
+        if (cache[n] != null) {
+            return cache[n];
+        }
+        ArrayList<String> ans = new ArrayList<String>();
+        // 递归开始
+        if (n == 0) {
+            ans.add("");
+        } else {
+
+            for (int c = 0; c < n; ++c) {
+                for (String left: generate(c)) {
+                    for (String right: generate(n - 1 - c)) {
+                        ans.add("(" + left + ")" + right);
+                    }
+                }
+            }
+        }
+        cache[n] = ans;
+        return ans;
+    }
+
+    public List<String> generateParenthesis(int n) {
+        return generate(n);
+    }
+
 }
