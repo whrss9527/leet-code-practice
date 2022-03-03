7aaea8aef1fe57c0d21839bedfd6180adde860cb
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2022-03-03 19:30:52 +0800

    22.括号生成 注释

--- src/medium/_22_括号生成.java
+++ src/medium/_22_括号生成.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class _22_括号生成 {
 
     ArrayList[] cache = new ArrayList[100];
-
+    // 递归 性能较差
     public List<String> generate(int n) {
         if (cache[n] != null) {
             return cache[n];
@@ -16,7 +16,9 @@ public class _22_括号生成 {
         if (n == 0) {
             ans.add("");
         } else {
-
+            // (a)b 的原则
+            // 递归调用 generate(i) 即可计算 a 的所有可能性
+            // 递归调用 generate(n - i - 1) 即可计算 b 的所有可能性
             for (int c = 0; c < n; ++c) {
                 for (String left: generate(c)) {
                     for (String right: generate(n - 1 - c)) {
